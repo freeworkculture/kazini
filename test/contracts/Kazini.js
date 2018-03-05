ff88c14535dee8fc03b5c7edee4b432a39355f2c
cohabo <cohabo@gmail.com>
2018-03-05 21:34:56 +0300

    Code Clinic
    CIT Boundary

---
+++ test/contracts/Kazini.js
@@ -0,0 +1,239 @@
+var Factor = artifacts.require("Factor");
+var Database = artifacts.require("Database");
+
+const contractName = '0x4461746162617365000000000000000000000000000000000000000000000000'
+const keyId = 0x9BCB2540EBAC30FC9E9EFF3D259B64A2
+const Base = 2;
+const _intention = '_intention_';
+const _serviceId = '_serviceId_';
+const _preCondition = '_preCondition_';
+const _goal = '_goal_';
+const _status = true;
+const _postCondition = [_goal,_status];
+const _time =  0;
+const _budget = 1000;
+const _projectUrl = '_projectUrl_';
+const _state = 2;
+const _preQualification = '_preQualification_';
+
+function assertThrow(err, test, msg) {
+  if (err.toString().indexOf(test) !== -1) {
+    assert(true, msg);
+  } else {
+    assert(false, err.toString())
+  }
+}
+
+contract("DoPromise", function(accounts) {
+
+  var _intention_ = 'intention_';
+  var _serviceId_ = 'serviceId_';
+  var _preCondition_ = '0x5f707265436f6e646974696f6e5f000000000000000000000000000000000000';
+  var _goal_ = '0x5f676f616c5f0000000000000000000000000000000000000000000000000000';
+  var _status_ = true;
+  var _postCondition_ = [_goal_,_status_];
+  let _time_ =  0;
+  let _budget_ = 1000;
+  var _projectUrl_ = '0x5f70726f6a65637455726c5f0000000000000000000000000000000000000000';
+  let _state_ = 2;
+  var _preQualification_ = 'preQualification_';
+
+  it("should initialise plans", function() {
+    var timestamp = Math.round(new Date().getTime()/1000);
+    var goal_;
+    var status_;
+    var preCondition_;
+    var time_;
+    var budget_;
+    var projectUrl_;
+    var creator_;
+    var curator_; 
+    return Database.deployed().then(function(instance) {
+      database = instance;
+      return database.initPlan(
+        _intention,_serviceId,_preCondition,_goal,_status,_projectUrl,creator,_preQualification,
+        {from: creator, value: 1});
+      }).then(function() {
+        return database.planState(_intention);
+      }).then(function(state_) {
+        assert.equal(state_, _state_);
+      }).then(function() {
+        return database.planGoal(_intention);
+      }).then(function(postcondition_) {
+        goal_ = postcondition_[0];
+        status_ = postcondition_[1];
+      }).then(function() {
+        assert.equal(goal_, _goal_);
+      }).then(function() {
+        assert.equal(status_, _status_);
+      }).then(function() {
+        return database.planProject(_intention);
+      }).then(function(project_) {
+        preCondition_ = project_[0];
+        time_ = project_[1].toNumber();
+        // console.log(time_.toString(10));
+        budget_ = project_[2].toNumber();
+        // console.log(budget_.toString(10));
+        projectUrl_ = project_[3];
+        creator_ = project_[4];
+        curator_ = project_[5];
+      }).then(function() {
+        assert.equal(preCondition_, _preCondition_);
+      }).then(function() {
+        assert.equal(time_, _time_);
+      }).then(function() {
+        assert.equal(budget_, _budget_);
+      }).then(function() {
+        assert.equal(projectUrl_, _projectUrl_);
+      }).then(function() {
+        assert.equal(creator_, creator);
+      }).then(function() {
+        assert.equal(curator_, 0);
+      });
+    });
+  it("should set deployer and doers", function() {
+    var contract;
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.getDeployer.call();
+    }).then(function(addr) {
+      assert.equal(addr, accounts[0]);
+    }).then(function() {
+      return contract.getDoers.call();
+    }).then(function(addr) {
+      assert.equal(addr, Doers.address);
+    });
+  });
+
+  it("should initially not have any promises", function() {
+    var contract;
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.getPromiseCount.call();
+    }).then(function(count) {
+      assert.equal(count, 0);
+    });
+  });
+
+  it("should initially not have any fulfillments", function() {
+    var contract;
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.getFulfillmentCount.call();
+    }).then(function(count) {
+      assert.equal(count, 0);
+    });
+  });
+
+  it("should only let doers promise something", function() {
+    var contract;
+    var timestamp = Math.round(new Date().getTime()/1000);
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.promise("foo", timestamp + 1, {from: accounts[0], value: 1});
+    }).then(function() {
+      return contract.getPromiseCount.call();
+    }).then(function(count) {
+      assert.equal(count, 1);
+    }).then(function() {
+      return contract.promise("bar", timestamp + 1, {from: accounts[2], value: 1});
+    }).catch(function(err) {
+      assertThrow(err, "invalid opcode");
+    }).then(function() {
+      return contract.getPromiseCount.call();
+    }).then(function(count) {
+      assert.equal(count, 1);
+    });
+  });
+
+  it("should only accept promises in the future", function() {
+    var contract;
+    var timestamp = Math.round(new Date().getTime()/1000);
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.promise("baz", timestamp - 1, {from: accounts[0], value: 1});
+    }).catch(function(err) {
+      assertThrow(err, "invalid opcode");
+    }).then(function() {
+      return contract.getPromiseCount.call();
+    }).then(function(count) {
+      assert.equal(count, 1);
+      assert.equal(web3.eth.getBalance(contract.address), 1);
+    });
+  });
+
+  it("should only accept promises of more than 0 Wei", function() {
+    var contract;
+    var timestamp = Math.round(new Date().getTime()/1000);
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.promise("baz", timestamp + 1, {from: accounts[0], value: 0});
+    }).catch(function(err) {
+      assertThrow(err, "invalid opcode");
+    }).then(function() {
+      return contract.getPromiseCount.call();
+    }).then(function(count) {
+      assert.equal(count, 1);
+    });
+  });
+
+  it("should only let doers fulfill a promise", function() {
+    var contract;
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.fulfill("foo", "123", {from: accounts[0]});
+    }).then(function() {
+      return contract.getFulfillmentCount.call();
+    }).then(function(count) {
+      assert.equal(count, 1);
+    }).then(function() {
+      return contract.fulfill("foo", "123", {from: accounts[2]});
+    }).catch(function(err) {
+      assertThrow(err, "invalid opcode");
+    }).then(function() {
+      return contract.getFulfillmentCount.call();
+    }).then(function(count) {
+      assert.equal(count, 1);
+    });
+  });
+
+  it("should only allow fulfillment of existing promises", function() {
+    var contract;
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.fulfill("bar", "123", {from: accounts[0]});
+    }).catch(function(err) {
+      assertThrow(err, "invalid opcode");
+    }).then(function() {
+      return contract.getFulfillmentCount.call();
+    }).then(function(count) {
+      assert.equal(count, 1);
+    });
+  });
+
+  it("should only allow fulfillment within expiry", function() {
+    var contract;
+    var timestamp = Math.round(new Date().getTime()/1000);
+    return DoPromise.deployed().then(function(instance) {
+      contract = instance;
+      return contract.promise("foo", timestamp + 1, {from: accounts[0], value: 1});
+    }).then(function() {
+      // Wait past the promise expiry time.
+      var ms = new Date().getTime();
+      while(new Date().getTime() < ms + 1500){ /* do nothing */ }
+      // Fulfill promise too late.
+      return contract.fulfill("foo", "123", {from: accounts[0]});
+    }).catch(function(err) {
+      assertThrow(err, "invalid opcode");
+    }).then(function() {
+      return contract.getPromiseCount.call();
+    }).then(function(count) {
+      assert.equal(count, 2);
+      assert.equal(web3.eth.getBalance(contract.address), 2);
+    }).then(function() {
+      return contract.getFulfillmentCount.call();
+    }).then(function(count) {
+      assert.equal(count, 1);
+    });
+  });
+});
---
+++ test/contracts/Reserve.js
@@ -0,0 +1,222 @@
+/* eslint-env mocha */
+/* eslint-disable no-await-in-loop */
+const TestRPC = require('ethereumjs-testrpc');
+const Web3 = require('web3');
+const chai = require('chai');
+
+const Reserve = artifacts.require("Reserve");
+const usingOraclize = artifacts.require("usingOraclize");
+const DoitToken = artifacts.require("DoitToken");
+const DoitTokenFactory =  artifacts.require("DoitTokenFactory");
+// const DoitTokenState = artifacts.require("DoitTokenState");
+
+const assert = chai.assert;
+const { utils } = Web3;
+
+const verbose = false;
+
+const log = (S) => {
+  if (verbose) {
+    console.log(S);
+  }
+};
+
+// b[0]  ->  0, 0, 0, 0
+// b[1]  ->  0,10, 0, 0
+// b[2]  ->  0, 8, 2, 0
+// b[3]  ->  0, 9, 1, 0
+// b[4]  ->  0, 6, 1, 0
+//  Clone token
+// b[5]  ->  0, 6, 1, 0
+// b[6]  ->  0, 2, 5. 0
+
+describe('DoitToken test', () => {
+  let testrpc;
+  let web3;
+  let accounts;
+  let doitToken;
+  let doitTokenState;
+  let doitTokenClone;
+  let doitTokenCloneState;
+  const b = [];
+
+  before(async () => {
+    testrpc = TestRPC.server({
+      ws: true,
+      gasLimit: 5800000,
+      total_accounts: 10,
+    });
+
+    testrpc.listen(8546, '127.0.0.1');
+
+    web3 = new Web3('ws://localhost:8546');
+    accounts = await web3.eth.getAccounts();
+  });
+
+  after((done) => {
+    testrpc.close();
+    done();
+  });
+
+  it('should deploy all the contracts', async () => {
+    const tokenFactory = await DoitTokenFactory.new(web3);
+    doitToken = await DoitToken.new(web3,
+      tokenFactory.$address,
+      0,
+      0,
+      'DOIT Test Token',
+      18,
+      'MMT',
+      0,
+      true);
+    assert.ok(doitToken.$address);
+    doitTokenState = new DoitTokenState(doitToken);
+  }).timeout(20000);
+
+  it('Should generate tokens for address 1', async () => {
+    b[0] = await web3.eth.getBlockNumber();
+    log(`b[0]-> ${b[0]}`);
+
+    await doitToken.generateTokens(accounts[1], 10);
+    const st = await doitTokenState.getState();
+    assert.equal(st.totalSupply, 10);
+    assert.equal(st.balances[accounts[1]], 10);
+    b[1] = await web3.eth.getBlockNumber();
+  }).timeout(6000);
+
+  it('Should transfer tokens from address 1 to address 2', async () => {
+    await doitToken.transfer(accounts[2], 2, { from: accounts[1], gas: 200000 });
+    b[2] = await web3.eth.getBlockNumber();
+    log(`b[2]->  ${b[3]}`);
+    const st = await doitTokenState.getState();
+    assert.equal(st.totalSupply, 10);
+    assert.equal(st.balances[accounts[1]], 8);
+    assert.equal(st.balances[accounts[2]], 2);
+
+    const balance = await doitToken.balanceOfAt(accounts[1], b[1]);
+    assert.equal(balance, 10);
+  }).timeout(6000);
+
+  it('Should allow and transfer tokens from address 2 to address 1 allowed to 3', async () => {
+    await doitToken.approve(accounts[3], 2, { from: accounts[2] });
+    const allowed = await doitToken.allowance(accounts[2], accounts[3]);
+    assert.equal(allowed, 2);
+
+    await doitToken.transferFrom(accounts[2], accounts[1], 1, { from: accounts[3] });
+
+    const allowed2 = await doitToken.allowance(accounts[2], accounts[3]);
+    assert.equal(allowed2, 1);
+
+    b[3] = await web3.eth.getBlockNumber();
+    log(`b[3]->  ${b[3]}`);
+    const st = await doitTokenState.getState();
+    assert.equal(st.totalSupply, 10);
+    assert.equal(st.balances[accounts[1]], 9);
+    assert.equal(st.balances[accounts[2]], 1);
+
+    let balance;
+
+    balance = await doitToken.balanceOfAt(accounts[1], b[2]);
+    assert.equal(balance, 8);
+    balance = await doitToken.balanceOfAt(accounts[2], b[2]);
+    assert.equal(balance, 2);
+    balance = await doitToken.balanceOfAt(accounts[1], b[1]);
+    assert.equal(balance, 10);
+    balance = await doitToken.balanceOfAt(accounts[2], b[1]);
+    assert.equal(balance, 0);
+    balance = await doitToken.balanceOfAt(accounts[1], b[0]);
+    assert.equal(balance, 0);
+    balance = await doitToken.balanceOfAt(accounts[2], b[0]);
+    assert.equal(balance, 0);
+    balance = await doitToken.balanceOfAt(accounts[1], 0);
+    assert.equal(balance, 0);
+    balance = await doitToken.balanceOfAt(accounts[2], 0);
+    assert.equal(balance, 0);
+  });
+
+  it('Should Destroy 3 tokens from 1 and 1 from 2', async () => {
+    await doitToken.destroyTokens(accounts[1], 3, { from: accounts[0], gas: 200000 });
+    b[4] = await web3.eth.getBlockNumber();
+    log(`b[4]->  ${b[4]}`);
+    const st = await doitTokenState.getState();
+    assert.equal(st.totalSupply, 7);
+    assert.equal(st.balances[accounts[1]], 6);
+  });
+
+  it('Should Create the clone token', async () => {
+    const doitTokenCloneTx = await doitToken.createCloneToken(
+      'Clone Token 1',
+      18,
+      'MMTc',
+      0,
+      0,
+      true);
+
+    let addr = doitTokenCloneTx.events.NewCloneToken.raw.topics[1];
+    addr = `0x${addr.slice(26)}`;
+    addr = utils.toChecksumAddress(addr);
+    doitTokenClone = new DoitToken(web3, addr);
+
+    doitTokenCloneState = new DoitTokenState(doitTokenClone);
+
+    b[5] = await web3.eth.getBlockNumber();
+    log(`b[5]->  ${b[5]}`);
+    const st = await doitTokenCloneState.getState();
+
+    assert.equal(st.parentToken, doitToken.$address);
+    assert.equal(st.parentSnapShotBlock, b[5]);
+    assert.equal(st.totalSupply, 7);
+    assert.equal(st.balances[accounts[1]], 6);
+
+    const totalSupply = await doitTokenClone.totalSupplyAt(b[4]);
+
+    assert.equal(totalSupply, 7);
+
+    const balance = await doitTokenClone.balanceOfAt(accounts[2], b[4]);
+    assert.equal(balance, 1);
+  }).timeout(6000);
+
+  it('Should mine one block to take effect clone', async () => {
+    await doitToken.transfer(accounts[1], 1, { from: accounts[1] });
+  });
+
+  it('Should move tokens in the clone token from 2 to 3', async () => {
+    await doitTokenClone.transfer(accounts[2], 4, { from: accounts[1] });
+    b[6] = await web3.eth.getBlockNumber();
+    log(`b[6]->  ${b[6]}`);
+
+    const st = await doitTokenCloneState.getState();
+    assert.equal(st.totalSupply, 7);
+    assert.equal(st.balances[accounts[1]], 2);
+    assert.equal(st.balances[accounts[2]], 5);
+
+    let balance;
+
+    balance = await doitToken.balanceOfAt(accounts[1], b[5]);
+    assert.equal(balance, 6);
+    balance = await doitToken.balanceOfAt(accounts[2], b[5]);
+    assert.equal(balance, 1);
+    balance = await doitTokenClone.balanceOfAt(accounts[1], b[5]);
+    assert.equal(balance, 6);
+    balance = await doitTokenClone.balanceOfAt(accounts[2], b[5]);
+    assert.equal(balance, 1);
+    balance = await doitTokenClone.balanceOfAt(accounts[1], b[4]);
+    assert.equal(balance, 6);
+    balance = await doitTokenClone.balanceOfAt(accounts[2], b[4]);
+    assert.equal(balance, 1);
+
+    let totalSupply;
+    totalSupply = await doitTokenClone.totalSupplyAt(b[5]);
+    assert.equal(totalSupply, 7);
+    totalSupply = await doitTokenClone.totalSupplyAt(b[4]);
+    assert.equal(totalSupply, 7);
+  }).timeout(6000);
+
+  it('Should create tokens in the child token', async () => {
+    await doitTokenClone.generateTokens(accounts[1], 10, { from: accounts[0], gas: 300000 });
+    const st = await doitTokenCloneState.getState();
+    assert.equal(st.totalSupply, 17);
+    assert.equal(st.balances[accounts[1]], 12);
+    assert.equal(st.balances[accounts[2]], 5);
+  });
+});
