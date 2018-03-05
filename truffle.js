e718885c72f33d1d778adf18f90a83fb48f3de08
cohabo <cohabo@gmail.com>
2018-03-05 19:20:31 +0300

    Trufle config

--- truffle.js
+++ truffle.js
@@ -5,18 +5,18 @@ require('babel-polyfill')
 
 module.exports = {
   networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 8545,
-      network_id: '*', // Match any network id
-      // optional config values:
-      // gas: 6712388
-      // gasPrice
-      // from - default address to use for any transaction Truffle makes during migrations
-      // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
-      //          - function that returns a web3 provider instance (see below.)
-      //          - if specified, host and port are ignored.
-    },
+    // development: {
+    //   host: '127.0.0.1',
+    //   port: 8545,
+    //   network_id: '*', // Match any network id
+    //   // optional config values:
+    //   // gas: 6712388
+    //   // gasPrice
+    //   // from - default address to use for any transaction Truffle makes during migrations
+    //   // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+    //   //          - function that returns a web3 provider instance (see below.)
+    //   //          - if specified, host and port are ignored.
+    // },
     // add a new network definition that will self host TestRPC
     localtest: {
       provider: TestRPC.provider(),
